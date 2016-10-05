f09a95a6390324a711eebe036623529f9368662d
Angel Buil Ambau <angelpingel@gmail.com>
2016-10-05 21:10:16 +0200

    Add get greeting by id

--- routes/greetings.js
+++ routes/greetings.js
@@ -2,33 +2,32 @@ var express = require('express');
 var router = express.Router();
 var Greeting = require('../models/greeting');
 
-
 router.get('/', function(req, res) {
     Greeting.find({}, function(err, users) {
         if (err) throw err;
-
         res.json(users);
     });
-});
-
-router.get('/:id(\\d+)/', function(req, res) {
-  res.json( { message: 'Hi number ' + req.params.id } );
 });
 
 router.post('/', function(req, res) {
     var message = req.body.message;
     if(!message) return new Error('Greeting is empty');
-
     var newGreeting = Greeting({
       message: message
     });
-
     newGreeting.save(function(err) {
       if (err) throw err;
-
       console.log('Greeting created!');
       res.json('Greeting created!');
   })
+});
+
+router.get('/:id', function(req, res) {
+    var id = req.params.id;
+    Greeting.findById(id, function(err, user) {
+      if (err) throw err;
+      res.json(user);
+    });
 });
 
 module.exports = router;
